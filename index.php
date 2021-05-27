583789f66992724c5fd2052485f1dcc669090904
ElijahBretbacher <elijah.bretbacher@htl-wels.at>
2021-05-27 20:17:13 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Taldin Teths Magic Tutoring</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Welcome</a></li>
+							<li><a href="#one">Who we are</a></li>
+							<li><a href="#two">What we do</a></li>
+							<li><a href="#three">Get in touch</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>Teths Magic Tutoring</h1>
+                            <p>Are thou struggling with magic classes? Do thee have a hard time understanding barbarian rage or how to get thy epic flute to work?
+                                Or do thee just want to find a better place to study the books of thy ancient pantheon? Whatever the reason may be,
+                                we guarantee that thou will most certainly find a course that will further thy adventuring career.</p>
+							<ul class="actions">
+								<li><a href="#one" class="button scrolly">Learn more</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<a href="#" class="image"><img src="images/pic01.jpg" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Classes</h2>
+									<p>A detailed list of all the classes my team of professionals teach, ranging from elemental magic over bardic magic to spacial magic.</p>
+									<ul class="actions">
+										<li><a href="generic.php" class="button">Learn more</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/pic02.jpg" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>The Elders</h2>
+									<p>We introduce our team of highly trained professionals. They were once Selenias most feared adventurers, having conquered countless dragons,
+                                        healed entire armies, performed in front of the biggest crowds and traded their way into fortune.</p>
+									<ul class="actions">
+										<li><a href="generic.php" class="button">Learn more</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/pic03.jpg" alt="" data-position="25% 25%" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>News</h2>
+									<p>The latest achievements of our instructors, students and ex-students. We keep in touch with long-term customers
+                                    and we are proud of each and every accomplishment they've made.</p>
+									<ul class="actions">
+										<li><a href="generic.php" class="button">Learn more</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>What are you looking for?</h2>
+							<p></p>
+							<div class="features">
+								<section>
+									<span class="icon solid major fa-code"></span>
+									<h3>Downloads</h3>
+									<p>From learning material to spell-lists. Everything you need in order to get started.</p>
+                                    <ul class="actions">
+                                        <li><a href="generic.php" class="button">Learn more</a></li>
+                                    </ul>
+                                </section>
+								<section>
+									<span class="icon solid major fa-lock"></span>
+									<h3>Location</h3>
+									<p>Where are we located and how do you get here? If you're tired of travelling just remember, you can always learn spacial magic around here.</p>
+                                    <ul class="actions">
+                                        <li><a href="generic.php" class="button">Learn more</a></li>
+                                    </ul>
+                                </section>
+								<section>
+									<span class="icon solid major fa-cog"></span>
+									<h3>Pricing</h3>
+									<p>Prices vary from class to class. Find out what your profession will cost you.</p>
+                                    <ul class="actions">
+                                        <li><a href="generic.php" class="button">Learn more</a></li>
+                                    </ul>
+                                </section>
+								<section>
+									<span class="icon solid major fa-desktop"></span>
+									<h3>Calendar</h3>
+									<p>Want to know when classes are scheduled? Look no further. Here you can find everything you need for the current year.</p>
+                                    <ul class="actions">
+                                        <li><a href="generic.php" class="button">Learn more</a></li>
+                                    </ul>
+                                </section>
+								<section>
+									<span class="icon solid major fa-link"></span>
+									<h3>Heroes</h3>
+									<p>A few examples of the heroes we helped on their way to glory.</p>
+                                    <ul class="actions">
+                                        <li><a href="generic.php" class="button">Learn more</a></li>
+                                    </ul>
+                                </section>
+								<section>
+									<span class="icon major fa-gem"></span>
+									<h3>AGB</h3>
+									<p>In case you injure yourself in the lessons.</p>
+                                    <ul class="actions">
+                                        <li><a href="generic.php" class="button">Learn more</a></li>
+                                    </ul>
+                                </section>
+							</div>
+						</div>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style1 fade-up">
+						<div class="inner">
+							<h2>Contact us</h2>
+							<p>Business inquires, questions regarding classes and other matters.</p>
+							<div class="split style1">
+								<section>
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<label for="name">Name</label>
+												<input type="text" name="name" id="name" />
+											</div>
+											<div class="field half">
+												<label for="email">Email</label>
+												<input type="text" name="email" id="email" />
+											</div>
+											<div class="field">
+												<label for="message">Message</label>
+												<textarea name="message" id="message" rows="5"></textarea>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><a href="" class="button submit">Send Message</a></li>
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+										<li>
+											<h3>Address</h3>
+											<span>Tethstreet 3<br />
+											Varbon Harge, Zvaaraes<br />
+											Montoora</span>
+										</li>
+										<li>
+											<h3>Email</h3>
+											<a href="#">support@teths-magic.mt</a>
+										</li>
+										<li>
+											<h3>Phone</h3>
+											<span>0649 84166848</span>
+										</li>
+										<li>
+											<h3>Social</h3>
+											<ul class="icons">
+												<li><a href="https://github.com/ElijahBretbacher" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+											</ul>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
